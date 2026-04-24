89bdf3b6eab273aaa6506dfaa86f545d3460148c
unknown <workinhim@gmail.com>
2026-04-25 00:23:04 +0100

    fixing dependencies

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,13 @@
-import "dotenv/config"; // Important: Loads your .env variables
+import "dotenv/config";
+import path from "node:path";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: path.join("prisma", "schema.prisma"),
+  migrations: {
+    path: path.join("prisma", "migrations"),
+  },
   datasource: {
-    url: env("DATABASE_URL"), // The CLI now looks here for the connection string
+    url: env("DATABASE_URL"),
   },
 });
